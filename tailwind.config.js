f930d9ac999175a612f084461b965cc0c5bf9524
ERRORthenBSOD <atmobsod@gmail.com>
2020-12-09 17:12:21 +0300

    upload all

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  purge: ['./pages/**/*.js', './components/**/*.js'],
+  darkMode: 'media',
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        128: '32rem',
+        144: '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  corePlugins: {
+    preflight: false,
+  },
+  plugins: [],
+};
